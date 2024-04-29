2154b153f32068c2f9ea337ded7c03b79aa85017
Stanislav Dashkevitch <stasdaskevic6@gmail.com>
2024-04-29 22:01:57 +0300

    add user controller

--- src/common/base.controller.ts
+++ src/common/base.controller.ts
@@ -1,6 +1,7 @@
 import { LoggerService } from "../logger/logger.service";
 import { Router } from "express";
 import { IControllerRouter } from "./route.interface";
+import { Response } from "express";
 
 export abstract class BaseController {
   private readonly _router: Router
@@ -11,6 +12,19 @@ export abstract class BaseController {
 
   get router() {
     return this._router
+  }
+
+  public send<T>(res: Response, code: number, message: T) {
+    res.type('application/json')
+    return res.status(code).json(message)
+  }
+
+  public ok<T>(res: Response, message: T) {
+    return this.send(res, 200, message)
+  }
+
+  public created(res: Response) {
+    return res.sendStatus(201)
   }
 
   protected bindRoutes(routes: IControllerRouter[]) {
